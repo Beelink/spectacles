31a220df606024790950fccc5641b3d1c9bf007b
Eugene <40366303+Beelink@users.noreply.github.com>
2023-01-28 06:00:31 +0200

    Update base url

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,13 @@ import App from "./App.vue";
 import { VueQueryPlugin, VueQueryPluginOptions } from "@tanstack/vue-query";
 import routes from "@/routes";
 import "@/assets/styles/global.scss";
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 
 const app = createApp(App);
 
 // vue-router
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 });
 app.use(router);
